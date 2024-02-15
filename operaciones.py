a56c28ed62255b9dd9753b1c57406cb6d6a2bdcb
IgnacioCantero <ignaciocanterohernandez@hotmail.com>
2024-02-15 14:55:17 +0100

    Actualizo operaciones, pruebas y Actions

--- operaciones.py
+++ operaciones.py
@@ -1,7 +1,23 @@
-# suma.py
+# operaciones.py
 
 def sumar(a, b):
     return a + b
 
+def restar(a, b):
+    return a - b
+
+def multiplicar(a, b):
+    return a * b
+
+def dividir(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "¡ERROR: No se puede dividir por cero!"
+
 if __name__ == "__main__":
     print(sumar(5, 3))
+    print(restar(5, 3))
+    print(multiplicar(5, 3))
+    print(dividir(5, 3))
+    print(dividir(5, 0)) # Comprobar la división entre 0
--- test_operaciones.py
+++ test_operaciones.py
@@ -1,12 +1,28 @@
 import unittest
-from suma import sumar
+from operaciones import sumar, restar, multiplicar, dividir
 
-class test_sumar(unittest.TestCase):
+class test_operaciones(unittest.TestCase):
 
     def test_sumar(self):
         self.assertEqual(sumar(3, 2), 5)
         self.assertEqual(sumar(-1, 1), 0)
         self.assertEqual(sumar(-1, -1), -2)
+
+    def test_restar(self):
+        self.assertEqual(restar(3, 2), 1)
+        self.assertEqual(restar(-1, 1), -2)
+        self.assertEqual(restar(-1, -1), 0)
+
+    def test_multiplicar(self):
+        self.assertEqual(multiplicar(3, 2), 6)
+        self.assertEqual(multiplicar(-1, 1), -1)
+        self.assertEqual(multiplicar(-1, -1), 1)
+
+    def test_dividir(self):
+        self.assertEqual(dividir(3, 2), 1.5)
+        self.assertEqual(dividir(-1, 1), -1)
+        self.assertEqual(dividir(-1, -1), 1)
+        self.assertEqual(dividir(5, 0), "¡ERROR: No se puede dividir por cero!")
 
 if __name__ == '__main__':
     unittest.main()
